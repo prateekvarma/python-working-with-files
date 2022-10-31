90758ee46301b5ccee848cb9952be13bcd045260
prateekvarma <prtk.varma@gmail.com>
2022-10-31 16:48:17 -0600

    Reads NYC squirrel CSV data, extracts data and puts it into another CSV

---
+++ Squirrel NYC Data/main.py
@@ -0,0 +1,19 @@
+# Reads a large CSV file of NYC squirrels data, and extracts the number
+# of squirrels of each color, and creates CSV file of those numbers
+import pandas
+
+data = pandas.read_csv("./2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+gray_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(gray_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [gray_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
